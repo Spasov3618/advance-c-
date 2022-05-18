c3e03822ffcf67675cb665fdc5796bf5f3f9e5bd
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 13:25:44 +0300

    add

---
+++ Stacks and Queues - Exercise/06. Songs Queue/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Queue<string> queue = new Queue<string>(Console.ReadLine().Split(", "));
+            while (queue.Count > 0)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Play")
+                {
+                    queue.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    string name = command.Substring(4);
+                    if (queue.Contains(name))
+                    {
+                        Console.WriteLine($"{name} is already contained!");
+                    }
+                    else
+                    {
+                        queue.Enqueue(name);
+
+                    }
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine($"{string.Join(", ", queue)}");
+                }
+            }
+            Console.WriteLine("No more songs!");
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/07. Truck Tour/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStations = int.Parse(Console.ReadLine());
+
+            Queue<int> fuelLeft = new Queue<int>();
+
+            for (int i = 0; i < numberOfStations; i++)
+            {
+                int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                fuelLeft.Enqueue(input[0] - input[1]);
+            }
+
+            for (int i = 0; i < fuelLeft.Count; i++)
+            {
+                Queue<int> copyFuelLeft = new Queue<int>(fuelLeft);
+
+                int currentFuel = 0;
+
+
+                for (int j = 0; j < copyFuelLeft.Count; j++)
+                {
+                    currentFuel += copyFuelLeft.Dequeue();
+
+                    if (currentFuel < 0)
+                    {
+                        break;
+                    }
+
+                }
+
+                if (currentFuel >= 0)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+
+                fuelLeft.Enqueue(fuelLeft.Dequeue());
+            }
+        }
+    }
+}
