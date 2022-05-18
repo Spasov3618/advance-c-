097def95616b2e187f7a1dc3d532d78f5bc68581
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 09:36:41 +0300

    add

--- Stacks and Queues - Lab/7. Hot Potato/Program.cs
+++ Stacks and Queues - Lab/7. Hot Potato/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _7._Hot_Potato
 {
@@ -6,7 +7,21 @@ namespace _7._Hot_Potato
     {
         static void Main(string[] args)
         {
-           
+            Queue<string> queue = new Queue<string>(Console.ReadLine().Split());
+            int n = int .Parse(Console.ReadLine());
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i < n; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
         }
     }
-}
+        }
+    
+
