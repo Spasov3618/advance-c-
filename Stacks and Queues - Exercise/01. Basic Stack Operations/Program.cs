e6a363b8147a7ebba17d722ae783b98e6d2275ad
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 11:04:02 +0300

    add

---
+++ Stacks and Queues - Exercise/01. Basic Stack Operations/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int [] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+            List<int> list = new List<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToList());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(list[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
---
+++ Stacks and Queues - Exercise/02. Basic Queue Operations/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+            List<int> list = new List<int>(Console.ReadLine().Split(' ').Select(int.Parse).ToList());
+            Queue<int> stack = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Enqueue(list[i]);
+            }
+            for (int i = 0; i < s; i++)
+            {
+                stack.Dequeue();
+            }
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
