af1c25de5460983b826f2fd1839146bbbaa30314
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 10:07:12 +0300

    add

--- Stacks and Queues - Lab/8. Traffic Jam/Program.cs
+++ Stacks and Queues - Lab/8. Traffic Jam/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace _8._Traffic_Jam
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = int.Parse(Console.ReadLine());
+            Queue<string> queue = new Queue<string>();
+            string cmd;
+            int counter = 0;
+            while ((cmd = Console.ReadLine()) != "end")
+            {
+                if (cmd != "green")
+                {
+                    queue.Enqueue(cmd);
+                }
+                else if (cmd == "green")
+                {
+                    
+
+                    for (int i = 0; i < number; i++)
+                    {
+                    if (queue.Count>0)
+                        { 
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        counter++;
+                    }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"{counter} cars passed the crossroads.");
         }
     }
 }
