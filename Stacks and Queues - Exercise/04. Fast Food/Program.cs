2ac79f2e28414ac2617e22c35cccf91b95b18fd6
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 11:55:35 +0300

    add

--- Stacks and Queues - Exercise/04. Fast Food/Program.cs
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -8,6 +8,33 @@ namespace _04._Fast_Food
     {
         static void Main(string[] args)
         {
+            int food = int.Parse(Console.ReadLine());
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            Console.WriteLine(queue.Max());
+            int counter = queue.Count();
+            for (int i = 0; i < counter; i++)
+
+            {
+
+                if (queue.Peek() <= food)
+                {
+                    food -= queue.Peek();
+                    queue.Dequeue();
+                }
+                else 
+                {
+                    break;  
+                }
+            }
+            if (queue.Count == 0)
+            {
+            Console.WriteLine("Orders complete");
+
+            }
+            else
+            {
+                Console.WriteLine("Orders left: " + string.Join(" ", queue));
+            }
             
         }
     }
