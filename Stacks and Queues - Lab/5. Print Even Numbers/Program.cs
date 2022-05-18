9b2ba0865dd6bbee45d964f5b48420b68f24d04d
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 09:19:52 +0300

    add

---
+++ Stacks and Queues - Lab/5. Print Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> vs = new Queue<int>(Console.ReadLine().Split(" ").Select(int.Parse));
+            Queue<int> vs1 = new Queue<int>();
+            while (vs.Count>0)
+            {
+                if (vs.Peek() % 2 == 0)
+                {
+                    vs1.Enqueue(vs.Peek());
+                }
+                vs.Dequeue();
+            }
+            Console.Write($"{string.Join(", ", vs1)}");
+        }
+    }
+}
---
+++ Stacks and Queues - Lab/6. Supermarket/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Queue<string> queue = new Queue<string>();
+            string cmd;
+            while ((cmd = Console.ReadLine()) != "End")
+            {
+                if (cmd != "Paid")
+                {
+                    queue.Enqueue(cmd);
+                }
+                else if (cmd == "Paid")
+                {
+                    while(queue.Count > 0)
+                    {
+                    Console.WriteLine(queue.Dequeue());
+
+                    }
+                }
+            }
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
