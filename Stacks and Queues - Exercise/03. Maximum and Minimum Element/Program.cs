1b1925ea54662e69b47519a538f4889759ae42aa
Spasov3618 <svetlio_spasov@abv.bg>
2022-05-18 11:23:19 +0300

    add

--- Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
+++ Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _03._Maximum_and_Minimum_Element
 {
@@ -6,7 +8,48 @@ namespace _03._Maximum_and_Minimum_Element
     {
         static void Main(string[] args)
         {
-           
+           int n = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] cmn = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                int command = cmn[0];
+                if (command == 1)
+                {
+                    stack.Push(cmn[1]);
+                }
+                else if (command == 2)
+                {
+                    if (stack.Count > 0)
+                    {
+
+                    stack.Pop();
+                    }
+                }
+                else if (command == 3)
+                {
+                    if (stack.Count==0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(stack.Max());
+                    }
+                }
+                else if (command == 4)
+                {
+                    if (stack.Count == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine(stack.Min());
+                    }
+                }
+            }
+            Console.WriteLine($"{string.Join(", ", stack)}");
         }
     }
 }
---
+++ Stacks and Queues - Exercise/04. Fast Food/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
