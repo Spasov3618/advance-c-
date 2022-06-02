8c53bbb280b638170120d7e98be795d75e05934c
Spasov <svetlio_spasov@abv.bg>
2022-06-02 14:40:48 +0300

    add

---
+++ Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+           Car car = new Car();
+            car.Make = "VW";
+            car.Model = "Golf";
+            car.Year = 1992;
+            car.FuelQuantity = 50;
+            car.FuelConsumption = 0.07;
+            car.Drive(700);
+
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
---
+++ Defining Classes - Lab/03. Car Constructors/Car.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+
+        }
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+           
+
+        }
+        public Car(string make, string model, int year,double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+           FuelQuantity=fuelQuantity;
+           FuelConsumption=fuelConsumption;
+
+
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+       
+
+
+        public void Drive (double distance)
+        {
+           
+            if (FuelQuantity - (distance * FuelConsumption)  <0)
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+            else
+            {
+                FuelQuantity -= (distance * FuelConsumption);   
+            }
+        }
+        public string WhoAmI()
+        {
+            var result = new StringBuilder();
+
+            result.AppendLine($"Make: {this.Make}");
+            result.AppendLine($"Model: {this.Model}");
+            result.AppendLine($"Year: {this.Year}");
+            result.Append($"Fuel: {this.FuelQuantity:F2}L");
+
+            return result.ToString();
+        }
+    }
+}
