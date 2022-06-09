48f0869ead8d62136e302c642144cd74078288fa
Spasov <svetlio_spasov@abv.bg>
2022-06-09 14:00:18 +0300

    add

---
+++ Test Exam/01. Bakery Shop/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bakery_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> total = new Dictionary<string, int>
+            {
+                {"Muffin",0 },
+                {"Baguette",0 },
+                {"Bagel",0 },
+                {"Croissant",0 },
+            };
+            
+            Queue<double> water = new Queue<double>(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(double.Parse));
+            Stack<double> flour = new Stack<double>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(double.Parse));
+            while (water.Count>0 && flour.Count>0)
+            {
+                double curentWater = water.Peek();
+                double curentFlour = flour.Peek();
+                double percentWater = (((curentWater / (curentWater + curentFlour)) * 100));
+                switch (percentWater)
+                {
+                    case  40:
+                       
+                        water.Dequeue();
+                        flour.Pop();
+                        total["Muffin"]++;
+                        break;
+                    case 30:
+                       
+                        water.Dequeue();
+                        flour.Pop();
+                        total["Baguette"]++;
+                        break;
+                    case 20:
+                       
+                        water.Dequeue();
+                        flour.Pop();
+                        total["Bagel"]++;
+                        break;
+                    case 50:
+                       
+                        water.Dequeue();
+                        flour.Pop();
+                        total["Croissant"]++;
+                        break;
+
+                    default:
+                        flour.Pop();
+                        water.Dequeue();
+                        if (water.Count == 0)
+                        {
+                        double restFlour = curentFlour - curentWater;
+                        flour.Push(restFlour);
+
+                        }
+                        else if(flour.Count == 0)
+                        {
+                            double restWater = curentWater - curentFlour;
+                            water.Enqueue(restWater);
+                        }
+
+                        
+                        total["Croissant"]++;
+                        break;
+                }
+
+            }
+            foreach (var item in total.OrderByDescending(n => n.Value).ThenBy(n => n.Key))
+            {
+                if (item.Value>0)
+                {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+
+                }
+            }
+            if (water.Count==0)
+            {
+                Console.WriteLine("Water left: None");
+            }
+            else
+            {
+                Console.WriteLine($"Water left: {string.Join(", ",water)}");
+            }
+            if (flour.Count==0)
+            {
+                Console.WriteLine("Flour left: None");
+            }
+            else
+            {
+                Console.WriteLine($"Flour left: {string.Join(", ",flour)}");
+            }
+            
+        }
+    }
+}
---
+++ Test Exam/01. Blacksmith/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Blacksmith
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> total = new Dictionary<string, int>
+            {
+                {"Gladius",0 },
+                {"Shamshir",0 },
+                {"Katana",0 },
+                {"Sabre",0 },
+                {"Broadsword",0 },
+            };
+
+            Queue<int> steel = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> carbon = new Stack<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            while (steel.Count > 0 && carbon.Count > 0)
+            {
+                int curentSteel = steel.Peek();
+                int curentCarbon = carbon.Peek();
+                int percentWater = curentSteel + curentCarbon;
+                switch (percentWater)
+                {
+                    case 70:
+                        steel.Dequeue();
+                        carbon.Pop();
+                        total["Gladius"]++;
+                        break;
+                    case 80:
+
+                        steel.Dequeue();
+                        carbon.Pop();
+                        total["Shamshir"]++;
+                        break;
+                    case 90:
+
+                        steel.Dequeue();
+                        carbon.Pop();
+                        total["Katana"]++;
+                        break;
+                    case 110:
+
+                        steel.Dequeue();
+                        carbon.Pop();
+                        total["Sabre"]++;
+                        break;
+                    case 150:
+                        steel.Dequeue();
+                        carbon.Pop();
+                        total["Broadsword"]++;
+                        break;
+
+                    default:
+
+                        carbon.Pop();
+                        steel.Dequeue();
+                        carbon.Push(curentCarbon + 5);
+                        break;
+                }
+
+            }
+            foreach (var item in total)
+            {
+                if (item.Value == 0)
+                {
+                    total.Remove(item.Key);
+                }
+            }
+            if (total.Count>0)
+            {
+            Console.WriteLine($"You have forged {total.Values.Sum()} swords.");
+
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+
+            var steelLeft = steel.Count == 0 ? "none" : string.Join(", ", steel);
+            var carbonLeft = carbon.Count == 0 ? "none" : string.Join(", ", carbon);
+
+            Console.WriteLine($"Steel left: {steelLeft}");
+            Console.WriteLine($"Carbon left: {carbonLeft}");
+
+            foreach (var item in total.OrderBy(n =>n.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
