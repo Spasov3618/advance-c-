b864dce77761125e0230d0b15487accf68dac6b8
Spasov <svetlio_spasov@abv.bg>
2022-06-14 13:28:03 +0300

    add

---
+++ Test Exam/02. Armory/Program.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Armory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+            char[,] armory = new char[n,n];
+            int rowIndex = 0;
+            int colIndex = 0;
+            int goldCoins = 0;
+            int rowMirror1 = 0;
+            int colMirror1 = 0;
+            int rowMirror2 = 0;
+            int colMirror2 = 0;
+            int mirrorCount = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+                for (int col = 0; col < n; col++)
+                {
+                    armory[row,col] = input[col];
+
+                    if (armory[row,col] == 'A')
+                    {
+                        rowIndex = row;
+                        colIndex = col;
+                    }
+                    if (armory[row,col] == 'M')
+                    {
+                        if (mirrorCount==0)
+                        {
+                            rowMirror1 = row;
+                            colMirror1 = col;
+                            mirrorCount++;  
+                        }
+                        rowMirror2 = row;
+                        colMirror2 = col;
+                    }
+                   
+                }
+                                      
+            }
+
+            while (true)
+            {
+                string directions = Console.ReadLine();
+                armory[rowIndex, colIndex] = '-';
+
+                switch (directions)
+                {
+                    case "up":
+                        rowIndex--;
+                        if (isIndex(armory, rowIndex, colIndex))
+                        {
+
+
+                            if (armory[rowIndex, colIndex] == 'M')
+                            {
+                                if (rowIndex == rowMirror1 && colIndex == colMirror1)
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror2;
+                                    colIndex = colMirror2;
+                                    armory[rowIndex, colIndex] = 'A';
+
+                                }
+                                else
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror1;
+                                    colIndex = colMirror1;
+                                    armory[rowIndex, colIndex] = 'A';
+                                }
+                            }
+                            else if (char.IsDigit(armory[rowIndex, colIndex]))
+                            {
+                                int gold = int.Parse(armory[rowIndex, colIndex].ToString());
+                                goldCoins += gold;
+
+                                armory[rowIndex, colIndex] = 'A';
+
+
+                            }
+                            else
+                            {
+                                armory[rowIndex, colIndex] = 'A';
+                            }
+                        }
+                            break;
+                    case "down":
+                        rowIndex++;
+                        if (isIndex(armory, rowIndex, colIndex))
+                        {
+
+
+                            if (armory[rowIndex, colIndex] == 'M')
+                            {
+                                if (rowIndex == rowMirror1 && colIndex == colMirror1)
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror2;
+                                    colIndex = colMirror2;
+                                    armory[rowIndex, colIndex] = 'A';
+
+                                }
+                                else
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror1;
+                                    colIndex = colMirror1;
+                                    armory[rowIndex, colIndex] = 'A';
+                                }
+                            }
+                            else if (char.IsDigit(armory[rowIndex, colIndex]))
+                            {
+                                int gold = int.Parse(armory[rowIndex, colIndex].ToString());
+                                goldCoins += gold;
+
+                                armory[rowIndex, colIndex] = 'A';
+
+
+                            }
+                            else
+                            {
+                                armory[rowIndex, colIndex] = 'A';
+                            }
+                        }
+                            break;
+                    case "left":
+                        colIndex--;
+                        if (isIndex(armory, rowIndex, colIndex))
+                        {
+
+
+                            if (armory[rowIndex, colIndex] == 'M')
+                            {
+                                if (rowIndex == rowMirror1 && colIndex == colMirror1)
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror2;
+                                    colIndex = colMirror2;
+                                    armory[rowIndex, colIndex] = 'A';
+
+                                }
+                                else
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror1;
+                                    colIndex = colMirror1;
+                                    armory[rowIndex, colIndex] = 'A';
+                                }
+                            }
+                            else if (char.IsDigit(armory[rowIndex, colIndex]))
+                            {
+                                int gold = int.Parse(armory[rowIndex, colIndex].ToString());
+                                goldCoins += gold;
+
+                                armory[rowIndex, colIndex] = 'A';
+
+
+                            }
+                            else
+                            {
+                                armory[rowIndex, colIndex] = 'A';
+                            }
+                        }
+                            break;
+                    case "right":
+                        colIndex++;
+                        if (isIndex(armory, rowIndex, colIndex))
+                        {
+                            if (armory[rowIndex, colIndex] == 'M')
+                            {
+                                if (rowIndex == rowMirror1 && colIndex == colMirror1)
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror2;
+                                    colIndex = colMirror2;
+                                    armory[rowIndex, colIndex] = 'A';
+
+                                }
+                                else
+                                {
+                                    armory[rowIndex, colIndex] = '-';
+                                    rowIndex = rowMirror1;
+                                    colIndex = colMirror1;
+                                    armory[rowIndex, colIndex] = 'A';
+                                }
+                            }
+                            else if (char.IsDigit(armory[rowIndex, colIndex]))
+                            {
+                                int gold = int.Parse(armory[rowIndex, colIndex].ToString());
+                                goldCoins += gold;
+
+                                armory[rowIndex, colIndex] = 'A';
+
+
+                            }
+                            else
+                            {
+                                armory[rowIndex, colIndex] = 'A';
+                            }
+                        }
+                        break;
+                }
+
+                if (!isIndex(armory, rowIndex, colIndex))
+                {
+                    Console.WriteLine("I do not need more swords!");
+                    Console.WriteLine($"The king paid {goldCoins} gold coins.");
+                    pringingMatrix(armory);
+
+                }
+
+
+                if (goldCoins >= 65)
+                {
+                    Console.WriteLine("Very nice swords, I will come back for more!");
+                    Console.WriteLine($"The king paid {goldCoins} gold coins.");
+                    pringingMatrix(armory);
+
+                }
+            }
+                
+            
+        }
+
+       
+
+        public static bool isIndex(char[,] armory,int rowIndex, int colIndex)
+        {
+            return rowIndex >= 0 && rowIndex <= armory.GetLength(0) && colIndex >= 0 && colIndex <= armory.GetLength(1);
+        }
+        public static void pringingMatrix(char[,] matrix)
+        {
+            for (int r = 0; r < matrix.GetLength(0); r++)
+            {
+                for (int c = 0; c < matrix.GetLength(1); c++)
+                {
+                    Console.Write(matrix[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Test Exam/Drones/Airfield.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Drones
+{
+    public class Airfield
+    {
+        public Airfield(string name, int capacity, double landingStrip)
+        {
+            Name = name;
+            Capacity = capacity;
+            LandingStrip = landingStrip;
+            Drones = new List<Drone>();
+        }
+
+        public List<Drone> Drones { get; set; }
+        public string	Name { get; set; }
+        public int	Capacity { get; set; }
+        public double	LandingStrip { get; set; }
+
+        public int Count => Drones.Count;
+
+        public	string AddDrone(Drone drone)
+        {
+            if (Count<Capacity)
+            {
+                if (string.IsNullOrEmpty(drone.Name) || string.IsNullOrEmpty(drone.Brand)|| drone.Range < 5 || drone.Range > 15)
+                {
+                    return "Invalid drone."; 
+                }
+            }
+            else
+            {
+                return "Airfield is full.";
+            }
+            Drones.Add(drone);
+            return $"Successfully added {drone.Name} to the airfield.";
+        }
+        public	bool RemoveDrone(string name)
+        {
+            Drone search = Drones.FirstOrDefault(dr => dr.Name == name);
+            if (search != null)
+            {
+                Drones.Remove(search);
+                return true;
+            }
+            return false;
+
+        }
+        public	int RemoveDroneByBrand(string brand)
+        {
+            
+            List<Drone> search =this. Drones.Where(dr => dr.Brand != brand).ToList();
+            int count =this. Drones.Count - search.Count;
+           this. Drones = search.ToList();
+            return count;
+        }
+        public Drone FlyDrone(string name)
+        {
+            
+            Drone search = Drones.FirstOrDefault(dr => dr.Name == name);
+            if (search != null)
+                search.Available = false;
+
+            return search;
+          
+               
+
+           
+        }
+       public	List<Drone> FlyDronesByRange(int range)
+        {
+            List<Drone> search = this.Drones.FindAll(dr => dr.Range == range);
+            
+            return search;
+        }
+        public string Report()
+        {
+            var result = new StringBuilder();
+            result.AppendLine($"Drones available at { this.Name }:");
+
+            var dronesNotInFlight = this.Drones.Where(d => d.Available == true);
+            foreach (var drone in dronesNotInFlight)
+            {
+                result.AppendLine(drone.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
