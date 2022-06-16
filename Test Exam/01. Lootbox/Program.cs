baacfd02c22e287d664677d5c07e59d463c30d1a
Spasov <svetlio_spasov@abv.bg>
2022-06-16 15:45:40 +0300

    add

---
+++ Test Exam/01. Lootbox/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace _01._Lootbox
+{
+using System;
+using System.Collections.Generic;
+using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Queue<int> firstBox = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+           Stack<int> secondBox = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+            int total = 0;
+            while (firstBox.Any() && secondBox.Any())
+            {
+                int sum = firstBox.Peek() + secondBox.Peek();
+                if (sum %2 == 0)
+                {
+                    total += sum;
+                    firstBox.Dequeue();
+                    secondBox.Pop();
+                }
+                else
+                {
+                    firstBox.Enqueue(secondBox.Pop());
+                }
+            }
+            if (firstBox.Count==0)
+            {
+                Console.WriteLine("First lootbox is empty");
+            }
+            else if (secondBox.Count==0)    
+            {
+                Console.WriteLine("Second lootbox is empty");
+            }
+            if (total>=100)
+            {
+                Console.WriteLine($"Your loot was epic! Value: {total}");
+            }
+            else
+            {
+                Console.WriteLine($"Your loot was poor... Value: {total}");
+            }
+        }
+    }
+}
---
+++ Test Exam/SkiRental/SkiRental.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkiRental
+{
+    public class SkiRental
+    {
+        public SkiRental(string name, int capacity)
+        {
+           this. Name = name;
+           this. Capacity = capacity;
+           this. Data = new List<Ski>();
+        }
+        private List<Ski> data;
+        private string name;
+        private int capacity;
+
+        public int Count { get { return Data.Count; } }
+
+      public void  Add(Ski ski)
+        {
+            if (Count< Capacity)
+            {
+                Data.Add(ski);
+            }
+        }
+        public bool Remove(string manufacturer, string model)
+        {
+            Ski ski= Data.FirstOrDefault(n =>n.Manufacturer==manufacturer && n.Model==model);
+            if (ski == null)
+            {
+                return false;
+            }
+            Data.Remove(ski);
+            return true;
+        }
+       public Ski GetNewestSki()
+        {
+            //int ski = Data.Max(n => n.Year);
+            Ski newSki = Data.OrderByDescending(n => n.Year).First();
+            if (newSki == null)
+            {
+                return null;
+            }
+            return newSki;
+        }
+       public string GetSki(string manufacturer, string model)
+        {
+            var ski = Data.Select(n => n.Manufacturer == manufacturer && n.Model == model).ToList();
+            return ski.ToString();
+        }
+        public string	GetStatistics()
+        { 
+            var sb = new StringBuilder();
+            sb.AppendLine($"The skis stored in {Name}:");
+            foreach (var item in Data)
+            {
+                sb.AppendLine(item.ToString());
+            }
+            return sb.ToString();
+
+
+    }
+
+
+
+
+
+        public List<Ski> Data { get =>this. data; set =>this. data = value; }
+        public string Name { get =>this. name; set =>this. name = value; }
+        public int Capacity { get =>this. capacity; set =>this. capacity = value; }
+
+
+
+    }
+}
