805b76705f0efa8dc8d6bb1f369676a4c086cc53
Spasov <svetlio_spasov@abv.bg>
2022-06-16 12:13:37 +0300

    add

---
+++ Test Exam/01.Masterchef/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Masterchef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, int> dictionary = new SortedDictionary<string, int>
+            {
+                {"Dipping sauce",  0 },
+                {"Green salad",  0 },
+                {"Chocolate cake",  0 },
+                {"Lobster",  0 },
+           
+
+            };
+            Queue<int> ingredient = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).Where(n => n != 0 ));
+            Stack <int> freshness = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+
+            while (ingredient.Count>0 && freshness.Count>0)
+            {
+
+                int total = ingredient.Peek() * freshness.Peek();
+               
+                 if (total == 150)
+                {
+                    dictionary["Dipping sauce"]++;
+                    ingredient.Dequeue();
+                    freshness.Pop();
+                }
+                else if (total == 250)
+                {
+                    dictionary["Green salad"]++;
+                    ingredient.Dequeue();
+                    freshness.Pop();
+                }
+                else if (total == 300)
+                {
+                    dictionary["Chocolate cake"]++;
+                    ingredient.Dequeue();
+                    freshness.Pop();
+                }
+                else if (total == 400)
+                {
+                    dictionary["Lobster"]++;
+                    ingredient.Dequeue();
+                    freshness.Pop();
+                }
+                else
+                {
+                    freshness.Pop();
+                    int newInkr = ingredient.Dequeue()+5;
+                    ingredient.Enqueue(newInkr);
+                }
+
+            }
+            if (dictionary.Values.All(n => n>0))
+            {
+                Console.WriteLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+              
+            }
+                if (ingredient.Count>0)
+                {
+                    Console.WriteLine($"Ingredients left: {ingredient.Sum()}");
+                }
+
+            foreach (var item in dictionary)
+            {
+                if (item.Value>0)
+                {
+                Console.WriteLine($" # {item.Key} --> {item.Value}");
+
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Test Exam/02.Survivor/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+namespace _02.Survivor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowNum = int.Parse(Console.ReadLine());
+            string[][] matrix = new string[rowNum][];
+            int moneyMi = 0;
+            int MoneyAnadar = 0;
+            for (int i = 0; i < rowNum; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                matrix[i] = input;
+            }
+            string command;
+            while ((command = Console.ReadLine()) != "Gong")
+            {
+                string[] arg = command.Split();
+                int row = int.Parse(arg[1]);
+                int col = int.Parse(arg[2]);
+                if (arg[0] == "Find")
+                {
+                    if (isIndex(matrix, row, col) && matrix[row][col] == "T")
+                    {
+                        moneyMi++;
+                        matrix[row][col] = "-";
+                    }
+                }
+                else if (arg[0] == "Opponent")
+                {
+                    string direction = arg[3];
+                    if (isIndex(matrix, row, col))
+                    {
+                        if (matrix[row][col] == "T")
+                        {
+                            MoneyAnadar++;
+                            matrix[row][col] = "-";
+                        }
+                        if (direction == "up")
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                row--;
+                                if (isIndex(matrix,row,col))
+                                {
+                                    if (matrix[row][col] == "T")
+                                    {
+                                        MoneyAnadar++;
+                                        matrix[row][col] = "-";
+                                    }
+                                }
+
+                            }
+
+                        }
+                        else if (direction == "down")
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                row++;
+                                if (isIndex(matrix, row, col))
+                                {
+                                    if (matrix[row][col] == "T")
+                                    {
+                                        MoneyAnadar++;
+                                        matrix[row][col] = "-";
+                                    }
+                                }
+
+                            }
+                        }
+                        else if (direction == "left")
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                col--;
+                                if (isIndex(matrix, row, col))
+                                {
+                                    if (matrix[row][col] == "T")
+                                    {
+                                        MoneyAnadar++;
+                                        matrix[row][col] = "-";
+                                    }
+                                }
+
+                            }
+                        }
+                        else if (direction == "right")
+                        {
+                            for (int i = 0; i < 3; i++)
+                            {
+                                col++;
+                                if (isIndex(matrix, row, col))
+                                {
+                                    if (matrix[row][col] == "T")
+                                    {
+                                        MoneyAnadar++;
+                                        matrix[row][col] = "-";
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+            }
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine(string.Join(" ",matrix[i]));
+            }
+            Console.WriteLine($"Collected tokens: {moneyMi}");
+            Console.WriteLine($"Opponent's tokens: {MoneyAnadar}");
+        }
+
+        private static bool isIndex(string[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+    }
+}
