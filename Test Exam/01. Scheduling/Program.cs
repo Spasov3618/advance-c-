3ba6c6c5764d8e6d638e9972633e304fcc9a1fb2
Spasov <svetlio_spasov@abv.bg>
2022-06-22 12:50:21 +0300

    add

--- Test Exam/01. Scheduling/Program.cs
+++ Test Exam/01. Scheduling/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _01._Scheduling
 {
@@ -6,7 +9,33 @@ namespace _01._Scheduling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Stack<int> problem = new Stack<int>(Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Queue<int> queue = new Queue<int>(Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            int number = int.Parse(Console.ReadLine());
+
+            while (problem.Any() && queue.Any())
+            {
+                int currProblem =problem.Peek();
+                int currQueue = queue.Peek();
+                if (currProblem == number)
+                {
+                    Console.WriteLine($"Thread with value {currQueue} killed task {number}");
+                    Console.Write($"{string.Join(" ",queue)}");
+                    Console.WriteLine();
+                    return;
+                }
+                else if (currQueue>=currProblem)
+                {
+                    problem.Pop();
+                    queue.Dequeue();
+                }
+                else if (currProblem>currQueue)
+                {
+                    queue.Dequeue();
+                }
+
+
+            }
         }
     }
 }
---
+++ Test Exam/02. Garden/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Garden
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sizeMatrix = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[,] matrix = new int[sizeMatrix[0], sizeMatrix[1]];
+            string command;
+            
+            while ((command = Console.ReadLine()) != "Bloom Bloom Plow")
+            {
+                int[] cordinates = command.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                int row = cordinates[0];
+                int col = cordinates[1];
+                if (row >= 0 && row < sizeMatrix[0] && col >= 0 && col < sizeMatrix[1])
+                {
+                    matrix[row, col] = 1;
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        matrix[row, i]++;
+                        matrix[i, col]++;
+                    }
+                    matrix[row, col] -= 2;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                }
+            }
+            
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j]+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
